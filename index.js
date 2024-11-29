3892362b3d2d54b2fa26621330488ea923b59fc7
Taavi <taavi.orro@voco.ee>
2024-11-29 13:20:54 +0200

    Create APi error Handling

--- index.js
+++ index.js
@@ -9,23 +9,68 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')))
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 const key = '0a6b8708bb0f2bb769a61a7be374bef5'
-let city = 'Tartu'
+
+const getWeatherData = (city) => {
+    return new Promise((resolve, reject)=> {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+        fetch(url)
+        .then((response)=> {
+            return response.json()
+        })
+        .then((data)=> {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            const result = {
+                description : description,
+                city : city,
+                temp : temp, 
+                error: null
+            }
+            resolve(result)
+        })
+        .catch(error => {
+            reject(error)
+        })
+    })
+}
 
 app.get('/', (req, res)=> {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-    .then ((response) => {
-        return response.json()
+    let city ='Tartu'
+    getWeatherData(city)
+    .then ((data)=> {
+        res.render('index', data)
     })
+})
+
+app.post('/', (req, res)=> {
+    let city = req.body.cityname
+    getWeatherData(city)
     .then((data)=> {
-        let description = data.weather[0].description
-        let city = data.name
-        let temp = Math.round(parseFloat(data.main.temp)-273.15)
-        console.log(description)
-        console.log(city)
-        console.log(temp)
+        res.render('index', data)
     })
-    res.render('index')
+}) 
+
+app.all('/',(req,res)=> {
+    let city
+    if(req.method == 'GET'){
+        city = 'Tartu'
+    } else if (req.method == 'POST'){
+        city =req.body.cityname
+    }
+    getWeatherData(city)
+    .then ((data)=> {
+        res.render('index', data)
+    })
+    .catch(error => {
+        res.render('index', {
+            error: 'problem with getting data, try again...'
+        })
+    })
 })
 
 app.listen(3002)
